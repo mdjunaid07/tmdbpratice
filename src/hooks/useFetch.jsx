0e5656383e4af7dbc378124fbc5ab3fd52cd03b6
junaid <itsaboycse@gmail.com>
2024-11-28 17:28:26 +0530

    change 1

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -8,8 +8,7 @@ import { options } from "../utils/Options";
         async function getMovies() {
              const response = await fetch (`https://api.themoviedb.org/3/${apiPath}?language=en-US&page=1`,options);
              const data = await response.json();
-             setData(data.results);
-
+             setData(data.results);git add .
             
         }
         getMovies()
