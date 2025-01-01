1ddc3ba033f2afe79903d9535787a6231c3ab842
junaid <itsaboycse@gmail.com>
2025-01-01 21:49:10 +0530

    comment

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -14,12 +14,12 @@ const Card = ({movie}) => {
    <Link to={`/movie/${id}`}>
     <div className="max-w-sm mx-auto bg-white border border-gray-200 rounded-lg shadow mb-5">
       <img className="rounded-t-lg" src={image} alt="" />  
-      <div className="p-5">
+      {/* <div className="p-5">
         <h5 className="mb-2 text-2xl font-bold text-gray-900 line-clamp-1">
             {original_title}
         </h5>
         <p className="mb-3 font-normal text-gray-700 line-clamp-3">{overview}</p>
-      </div>
+      </div> */}
     </div>
    </Link>
   )
