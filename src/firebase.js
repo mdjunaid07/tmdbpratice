bea6a38aef30c043eba0650fc30fbc777f431dd9
junaid <itsaboycse@gmail.com>
2025-01-06 22:28:30 +0530

    addded a firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBkbxiY75R4EuJoUuIq_628fs66Cb6ki1E",
+  authDomain: "cinibyte-8207a.firebaseapp.com",
+  projectId: "cinibyte-8207a",
+  storageBucket: "cinibyte-8207a.firebasestorage.app",
+  messagingSenderId: "1009179362612",
+  appId: "1:1009179362612:web:7ec02cab97d1947835d161"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
